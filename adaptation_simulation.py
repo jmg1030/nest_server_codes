283c46fd3959dab3ba0fe378458259218436a759
Jonathan Gornet <jmg1030@nyu.edu>
2017-03-25 17:16:04 -0400

    Commit

--- adaptation_simulation.py
+++ adaptation_simulation.py
@@ -4,7 +4,7 @@ import numpy
 
 for b in range(0,105,5):
     for I in range(0,1010,10):
-        nest.SetKernelStatus({"local_num_threads":100})
+        nest.SetKernelStatus({"local_num_threads":36})
         data = neuron_functions.neuron_population([I], 50, b, 100, 100000)
         numpy.savetxt('Adaptation_' + str(b) + '_External_' + str(I) + '.csv', data, delimiter=',')
         nest.ResetKernel()
