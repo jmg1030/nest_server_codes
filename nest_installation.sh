e8580a54e7a3e7186e1c83f4e6d8befcff0ee779
Jonathan Gornet <jmg1030@nyu.edu>
2017-03-24 23:40:37 -0400

    Commit

--- nest_installation.sh
+++ nest_installation.sh
@@ -6,19 +6,13 @@ mkdir nest-2.8.0-build
 
 cd nest-2.8.0-build
 
-export PATH=$PATH:/home/jgornet/opt/nest/bin
-export PYTHONPATH=/home/jgornet/opt/nest/lib/python2.7/site-packages:$PYTHONPATH
-
-export PATH=$PATH:/home/jgornet/opt/nest/bin
-export PYTHONPATH=/home/jgornet/opt/nest/lib/python3.5/site-packages:$PYTHONPATH
+export PATH=$PATH:~/nest_server_codes/opt/nest/bin
+export PYTHONPATH=~/nest_server_codes/opt/nest/lib/python2.7/site-packages:$PYTHONPATH
 
 ../nest-2.8.0/configure --prefix=/home/jgornet/opt/nest
 
-export PATH=$PATH:/home/jgornet/opt/nest/bin
-export PYTHONPATH=/home/jgornet/opt/nest/lib/python2.7/site-packages:$PYTHONPATH
-
-export PATH=$PATH:/home/jgornet/opt/nest/bin
-export PYTHONPATH=/home/jgornet/opt/nest/lib/python3.5/site-packages:$PYTHONPATH
+export PATH=$PATH:~/nest_server_codes/opt/nest/bin
+export PYTHONPATH=~/nest_server_codes/opt/nest/lib/python2.7/site-packages:$PYTHONPATH
 
 make 
 make install
