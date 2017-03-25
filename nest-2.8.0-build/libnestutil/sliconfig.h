2c631d3187d2f19e250a42fe164fcc7e6060f0c7
Ubuntu <ubuntu@ip-172-31-42-51.us-west-2.compute.internal>
2017-03-25 17:17:29 +0000

    Commit

---
+++ nest-2.8.0-build/libnestutil/sliconfig.h
@@ -0,0 +1,35 @@
+#ifndef SLICONFIG_H
+#define SLICONFIG_H
+/* 
+    Configuration header.
+    sliconfig.h is automatically generated from sliconfig.h.in
+    during the configuration process. 
+
+    Please make all changes in sliconfig.h.in.
+*/
+
+#include "config.h"
+
+#define SLI_PRGNAME "nest-2.8.0"
+
+#define SLI_MAJOR_REVISION 2
+#define SLI_MINOR_REVISION 8
+#define SLI_PATCHLEVEL "0"
+
+#define SLI_HOST "x86_64-unknown-linux-gnu"
+#define SLI_HOSTOS "linux-gnu"
+#define SLI_HOSTCPU "x86_64"
+#define SLI_HOSTVENDOR "unknown"
+
+#define SLI_SOURCEDIR "/home/ubuntu/nest_server_codes/nest-2.8.0"
+#define SLI_BUILDDIR "/home/ubuntu/nest_server_codes/nest-2.8.0-build"
+#define SLI_PREFIX "/home/ubuntu/nest_server_codes/opt/nest"
+
+#define SLI_EXITCODE_ABORT 134
+#define SLI_EXITCODE_SEGFAULT 139
+
+/* Do not delete the empty line above ! Configure will not work correctly
+   on some systems without it!
+*/
+#endif
+/** end of file **/
